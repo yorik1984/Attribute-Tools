3b3baec68eb233be460b9801d75928f1a051940c
yorik1984 <yorik1984@gmail.com>
2021-07-22 21:57:11 +0300

    Revert "initial commit v.2"
    This reverts commit 5d68dc228b37b3a702095cf06c20e7095b005d50.

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-# Add attribute
+Add attribute
+====================
 
-# **_Changelog_**
+**_Changelog_**
 
-Jul
 21-July-2014     - *version 1.0 - Initial release*
 
-10-November-2015 - version 1.1-beta
- - add English version of dialog
+10-November-2015 - *version 1.1-beta*
+ - add english version of dialogs
  - add toolbar icon
  - validate input
 
-28-November-2015  - *version 1.2 -beta release*
+28-November-2015 - *version 1.2 -beta release*
  - change icon size
- - some bug fix
+ - some bugfix
  - add component nesting levels
  - add Help content
-30-November-2015 - *version 1.2 - First public version*
-5-November-2020 - *version 2.0 - New public version*
- - multiply attributes adding
- - load pre-settings
- - 
-# Description
 
+30-November-2015 - *version 1.2 - First public version*
+
+Description
+-----------
 Ruby plugin for Sketchup 2014 Pro or above is to define dynamic attributes of selected components of the model, including components in all levels of nested components.
 
 Plugin features :
@@ -31,8 +29,8 @@ Plugin features :
 * Using for components only
 * Dynamically change levels of nested components depending on the selection
 
-# Usage
-
+Usage
+-------
 You can use the plugin from toolbar or from the main menu.
 
 *Extensions \> Add attribute*
@@ -40,13 +38,15 @@ You can use the plugin from toolbar or from the main menu.
 1. **Add attributes inputbox** - Launch dialogbox for choosing what attributes may be added. More information see in help file of plugin.
 2. **Help** - Show the plugin manual.
 
-# Installation
+##### 
 
+Installation
+-----------
 Plugin works on Sketchup 2014 Pro and above. Update SketchUp Version before installing the plugin.
 [Installing Ruby Plugins in SketchUp](http://help.sketchup.com/en/article/38583)
 
-# Authors
-
+Authors
+-----------
 *Igor Sepelev aka [goga63](https://www.youtube.com/channel/UCYHOlAYFzT2R55uH7KDFIaQ/about)* - creator of 1-st version
 
 *Yurij Kulchevich aka [yorik1984](https://www.youtube.com/channel/UCwqnESLXeCXkN8KEoIkMoyA/about)* - author of improved features
--- src/add_attribute_loader.rb
+++ src/add_attribute_loader.rb
@@ -1,17 +1,18 @@
 # Loader for add_attribute/add_attribute.rb
 
-require 'sketchup'
-require 'extensions'
+require 'sketchup.rb'
+require 'extensions.rb'
 
-ver_req = 14
+version_required = 14
 
-if Sketchup.version.to_f >= ver_req
+if Sketchup.version.to_f >= version_required
   if Sketchup.is_pro?
 
     module AddAttributes
+
       PLUGIN_ID       = 'AddAttributes'.freeze
       PLUGIN_NAME     = 'Add attribute'.freeze
-      PLUGIN_VERSION  = '2.0'.freeze
+      PLUGIN_VERSION  = '1.2'.freeze
 
       FILENAMESPACE = File.basename(__FILE__, '_loader.rb')
       PATH_ROOT     = File.dirname(__FILE__).freeze
@@ -19,20 +20,22 @@ if Sketchup.version.to_f >= ver_req
       PATH_ICONS    = File.join(PATH, 'icons').freeze
 
       unless file_loaded?(__FILE__)
-        loader = File.join(PATH, FILENAMESPACE + '.rb')
+        loader = File.join( PATH, FILENAMESPACE + '.rb' )
         add_attribute             = SketchupExtension.new(PLUGIN_NAME, loader)
         add_attribute.description = 'Plugin add attributes of components in model'
         add_attribute.version     = PLUGIN_VERSION
-        add_attribute.copyright   = 'Copyright 2021 by Igor Sepelev and Yurij Kulchevich(english version, new features)'
+        add_attribute.copyright   = 'Copyright 2014 by Igor Sepelev aka goga63 and Yurij Kulchevich aka yorik1984 (english version, new features)'
         add_attribute.creator     = 'Igor Sepelev aka goga63'
         Sketchup.register_extension(add_attribute, true)
       end
+
     end
 
   else
-    UI.messagebox('Plugin "Add attribute" work only in PRO version of Sketchup. Visit sketchup.com to upgrade.')
+    UI.messagebox("Plugin \"Add attribute\" work only in PRO version of Sketchup. Visit sketchup.com to upgrade.")
   end
 else
-  UI.messagebox("Plugin «Add attribute» need Sketchup version 20#{ver_req} above. Visit sketchup.com to upgrade.")
+  UI.messagebox("Plugin \"Add attribute\" doesn't work in this version of Sketchup. Please, install Sketchup version 20#{version_required.to_s} or above to run this plugin. Visit sketchup.com to upgrade.")
 end
+
 file_loaded(__FILE__)
